f325d03271b3733ef0743c0de866b519b9c63af2
Chetan-108 <gangavane.chetan@gmail.com>
2022-09-21 23:49:37 +0530

    Adding a Grid Demo project

---
+++ Grid Demo/src/demo/GridEx.java
@@ -0,0 +1,46 @@
+package demo;
+
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class GridEx {
+
+	RemoteWebDriver driver=null;
+	String nodeUrl="http://localhost:4444/wd/hub";
+	
+	@Parameters("port")
+	@BeforeClass
+	public void setup(String port)throws Exception {
+		if(port.equals("4455")) {
+			ChromeOptions options= new ChromeOptions();
+			options.setCapability("browser", "Chrome");
+			options.setCapability("platform", "WINDOWS");	
+			driver= new RemoteWebDriver(new URL(nodeUrl),options);
+		}
+		if(port.equals("4456")) {
+			
+			InternetExplorerOptions options = new InternetExplorerOptions();
+			options.setCapability("browser", "edge");
+			options.setCapability("useAutomationExtension", false);
+			driver= new RemoteWebDriver(new URL(nodeUrl),options);
+		}
+		driver.get("file:///C:/javabykiran-Selenium-Softwares/Offline%20Website/index.html");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void test01() {
+		driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("123456");
+		driver.findElement(By.xpath("//button")).click();
+		Assert.assertEquals(driver.getTitle(), "JavaByKiran | Dashboard");
+	}
+}
